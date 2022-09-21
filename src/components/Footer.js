32ad80bb278d63999c2649a91048eb3976c0e57b
Faisal Almrwi <113801311+FaisalBesher@users.noreply.github.com>
2022-09-21 14:14:39 +0300

    Add files via upload

---
+++ src/components/Footer.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function Footer() {
+  return (
+    <div className='foteer'>
+        <h2>Contact with us </h2>
+       <a href= "https://twitter.com/tuwaiqacademy?s=21&t=IKPaANnkvYsOGG8nJ2Gdcg" >Twitter @TuwaiqAcademy</a>
+        <br></br>
+        <a href = "https://www.linkedin.com/company/tuwaiqacademy/" >Linkedln Tuwaiq Academy</a>
+      
+    </div>
+  )
+}
+
+export default Footer
---
+++ src/components/Nav.js
@@ -0,0 +1,26 @@
+import React from 'react'
+
+
+function Nav() {
+  return (
+    <div>
+
+<div className='ul-list'> 
+<h1>Couress</h1>
+<div>
+         <img height={"100px"} src = "https://i0.wp.com/linkedksa.com/wp-content/uploads/2022/06/29DEA9F2-F9A5-43BE-B22F-322F36F6B757.png?fit=400%2C335&ssl=1"></img>
+                
+                </div>
+        <ul>
+          
+            <li><button>Home</button></li>
+            <li><button>Courss</button></li>
+            <li><button>About</button></li>
+
+        </ul>
+        </div>
+    </div>
+  )
+}
+
+export default Nav
